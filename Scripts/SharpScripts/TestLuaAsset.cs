3a385614fb7db6a0cd240fb20ff252cc2a26eea8
li.yubin <liyubin724@163.com>
2019-12-05 01:28:05 +0800

    添加测试生成泛型

--- Scripts/SharpScripts/TestLuaAsset.cs
+++ Scripts/SharpScripts/TestLuaAsset.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -14,13 +15,32 @@ public class TestLuaAsset : MonoBehaviour
 {
     public LuaAsset luaAsset;
     // Start is called before the first frame update
+    List<int> list = new List<int>();
     void Start()
     {
         //AssetBundle ab = AssetBundle.LoadFromFile("E:/output/aa");
         //SceneManager.LoadScene("testscene");
         //ab.Unload(false);
-        Type t = typeof(TT);
-        Debug.Log(t.IsNotPublic);
+
+        Type tt = typeof(Dictionary<,>);
+
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        foreach(var a in assemblies)
+        {
+            Type[] types = a.GetTypes();
+            foreach(var t in types)
+            {
+                if(t.FullName == typeof(List<>).FullName)
+                {
+                    Debug.LogError(t.FullName);
+                }
+            }
+        }
+
+        Debug.Log(typeof(int).FullName);
+
+        Type newType = tt.MakeGenericType(typeof(int), typeof(string));
+        Debug.Log(newType.FullName);
     }
 
     // Update is called once per frame
