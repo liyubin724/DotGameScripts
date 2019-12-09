bcd587bfb819fc666f0d698e86ee21c8bbcdfbab
li.yubin <liyubin724@163.com>
2019-12-10 00:01:10 +0800

    调整目录结构，同时测试Lua启动

---
+++ Scripts/SharpScripts/Game/GameController.cs
@@ -0,0 +1,30 @@
+﻿using Dot;
+using Dot.Lua;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace Game
+{
+    public class GameController : MonoBehaviour
+    {
+        public LuaAsset[] preloadLuaAssets;
+        public LuaEnvType luaEnvType = LuaEnvType.Game;
+        public string luaMgrName = "LuaManager";
+
+        private void Awake()
+        {
+            DotProxy.StartUp();
+
+            string[] luaPathFormat = new string[]
+            {
+                LuaConfig.DefaultDiskPathFormat,
+            };
+
+            LuaManager.GetInstance().NewLuaEnv(luaEnvType, luaPathFormat,preloadLuaAssets, luaMgrName);
+        }
+    }
+}
+
