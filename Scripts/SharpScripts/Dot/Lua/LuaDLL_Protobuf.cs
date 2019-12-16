b765faac5cfa5bbf6ddc37a215632d096a04ff1e
li.yubin <liyubin724@163.com>
2019-12-17 00:37:42 +0800

    添加lua-protobuf支持，删除pbc

---
+++ Scripts/SharpScripts/Dot/Lua/LuaDLL_Protobuf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace XLua.LuaDLL
+{
+    public partial class Lua
+    {
+        [DllImport(LUADLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int luaopen_pb(IntPtr L);
+
+        [MonoPInvokeCallback(typeof(LuaDLL.lua_CSFunction))]
+        public static int LoadProtobuf(IntPtr L)
+        {
+            return luaopen_pb(L);
+        }
+    }
+}
