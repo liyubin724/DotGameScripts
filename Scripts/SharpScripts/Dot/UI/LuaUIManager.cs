3e7205faa8b61406dfbb92975e83b9303717739a
yubin.li <liyubin.724@163.com>
2019-12-20 19:02:03 +0800

    准备添加UI管理器与ListView实现

---
+++ Scripts/SharpScripts/Dot/UI/LuaUIManager.cs
@@ -0,0 +1,13 @@
+﻿using Dot.Lua.Register;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dot.UI
+{
+    public class LuaUIManager : LuaScriptBindBehaviour
+    {
+    }
+}
