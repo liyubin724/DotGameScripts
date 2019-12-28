00a4e10e4a54a9feba3b944f30d1ae94371d54d8
li.yubin <liyubin724@163.com>
2019-12-28 19:42:44 +0800

    删除不再需要的AB生成规则及Ab打包方式，切换使用新的方式

--- Scripts/SharpScripts/Editor/DotEditor/Core/BundleDepend/AssetDependFinder.cs
+++ Scripts/SharpScripts/Editor/DotEditor/Core/BundleDepend/AssetDependFinder.cs
@@ -1,139 +1,139 @@
-﻿using DotEditor.Core.Packer;
-using DotEditor.Util;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.U2D;
+﻿//using DotEditor.Core.Packer;
+//using DotEditor.Util;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using UnityEditor;
+//using UnityEngine;
+//using UnityEngine.U2D;
 
-namespace DotEditor.Core.BundleDepend
-{
-    public class AssetDependFinder
-    {
-        public Action<string, float> progressCallback;
-        private int findIndex = 0;
-
-
-        List<string> bundleAssetPathList = null;
-        Dictionary<string, string> spriteInAtlasDic = new Dictionary<string, string>();
-        Dictionary<string, List<string>> bundleDependAssetDic = new Dictionary<string, List<string>>();
-        Dictionary<string, int> assetUsedCountDic = new Dictionary<string, int>();
+//namespace DotEditor.Core.BundleDepend
+//{
+//    public class AssetDependFinder
+//    {
+//        public Action<string, float> progressCallback;
+//        private int findIndex = 0;
 
 
-        public void Find(AssetBundleTagConfig tagConfig)
-        {
-            bundleAssetPathList = (from groupData in tagConfig.groupDatas
-                                   from detailData in groupData.assetDatas
-                                   select detailData.assetPath).ToList();
-            findIndex = 0;
-            DealWithAtlas();
+//        List<string> bundleAssetPathList = null;
+//        Dictionary<string, string> spriteInAtlasDic = new Dictionary<string, string>();
+//        Dictionary<string, List<string>> bundleDependAssetDic = new Dictionary<string, List<string>>();
+//        Dictionary<string, int> assetUsedCountDic = new Dictionary<string, int>();
 
-            bundleAssetPathList.ForEach((path) =>
-            {
-                if(!(Path.GetExtension(path).ToLower() == ".spriteatlas"))
-                {
 
-                    progressCallback?.Invoke(path, findIndex / (float)bundleAssetPathList.Count);
-                    findIndex++;
+//        public void Find(AssetBundleTagConfig tagConfig)
+//        {
+//            bundleAssetPathList = (from groupData in tagConfig.groupDatas
+//                                   from detailData in groupData.assetDatas
+//                                   select detailData.assetPath).ToList();
+//            findIndex = 0;
+//            DealWithAtlas();
 
-                    List<string> depends = new List<string>();
-                    FindAssetDependExcludeBundle(path, depends, new string[] { ".cs" , ".shader"});
-                    bundleDependAssetDic.Add(path,depends);
-                }
-            });
+//            bundleAssetPathList.ForEach((path) =>
+//            {
+//                if(!(Path.GetExtension(path).ToLower() == ".spriteatlas"))
+//                {
 
-            foreach(var kvp in bundleDependAssetDic)
-            {
-                foreach(var path in kvp.Value)
-                {
-                    if(assetUsedCountDic.ContainsKey(path))
-                    {
-                        assetUsedCountDic[path]++;
-                    }else
-                    {
-                        assetUsedCountDic.Add(path, 1);
-                    }
-                }
-            }
-        }
+//                    progressCallback?.Invoke(path, findIndex / (float)bundleAssetPathList.Count);
+//                    findIndex++;
 
-        public Dictionary<string,int> GetRepeatUsedAssets()
-        {
-            Dictionary<string, int> result = new Dictionary<string, int>();
-            foreach(var kvp in assetUsedCountDic)
-            {
-                if(kvp.Value>1)
-                {
-                    result.Add(kvp.Key, kvp.Value);
-                }
-            }
-            return result;
-        }
+//                    List<string> depends = new List<string>();
+//                    FindAssetDependExcludeBundle(path, depends, new string[] { ".cs" , ".shader"});
+//                    bundleDependAssetDic.Add(path,depends);
+//                }
+//            });
 
-        public string[] GetBundleByUsedAsset(string assetPath)
-        {
-            List<string> result = new List<string>();
-            foreach (var kvp in bundleDependAssetDic)
-            {
-                if(kvp.Value.IndexOf(assetPath)>0)
-                {
-                    result.Add(kvp.Key);
-                }
-            }
-            return result.ToArray();
-        }
+//            foreach(var kvp in bundleDependAssetDic)
+//            {
+//                foreach(var path in kvp.Value)
+//                {
+//                    if(assetUsedCountDic.ContainsKey(path))
+//                    {
+//                        assetUsedCountDic[path]++;
+//                    }else
+//                    {
+//                        assetUsedCountDic.Add(path, 1);
+//                    }
+//                }
+//            }
+//        }
 
-        private void FindAssetDependExcludeBundle(string assetPath,List<string> depends, string[] excludeExtension)
-        {
-            string[] directDepends = AssetDatabase.GetDependencies(assetPath, false);
-            foreach(var path in directDepends)
-            {
-                if(path.StartsWith("Packages/"))
-                {
-                    continue;
-                }
-                string ext = Path.GetExtension(path).ToLower();
+//        public Dictionary<string,int> GetRepeatUsedAssets()
+//        {
+//            Dictionary<string, int> result = new Dictionary<string, int>();
+//            foreach(var kvp in assetUsedCountDic)
+//            {
+//                if(kvp.Value>1)
+//                {
+//                    result.Add(kvp.Key, kvp.Value);
+//                }
+//            }
+//            return result;
+//        }
+
+//        public string[] GetBundleByUsedAsset(string assetPath)
+//        {
+//            List<string> result = new List<string>();
+//            foreach (var kvp in bundleDependAssetDic)
+//            {
+//                if(kvp.Value.IndexOf(assetPath)>0)
+//                {
+//                    result.Add(kvp.Key);
+//                }
+//            }
+//            return result.ToArray();
+//        }
+
+//        private void FindAssetDependExcludeBundle(string assetPath,List<string> depends, string[] excludeExtension)
+//        {
+//            string[] directDepends = AssetDatabase.GetDependencies(assetPath, false);
+//            foreach(var path in directDepends)
+//            {
+//                if(path.StartsWith("Packages/"))
+//                {
+//                    continue;
+//                }
+//                string ext = Path.GetExtension(path).ToLower();
                 
-                if(path!=assetPath && Array.IndexOf(excludeExtension,ext) < 0 && bundleAssetPathList.IndexOf(path)<0 && 
-                    depends.IndexOf(path)<0 && !spriteInAtlasDic.ContainsKey(path))
-                {
-                    depends.Add(path);
+//                if(path!=assetPath && Array.IndexOf(excludeExtension,ext) < 0 && bundleAssetPathList.IndexOf(path)<0 && 
+//                    depends.IndexOf(path)<0 && !spriteInAtlasDic.ContainsKey(path))
+//                {
+//                    depends.Add(path);
 
-                    FindAssetDependExcludeBundle(path,depends,excludeExtension);
-                }
-            }
-        }
+//                    FindAssetDependExcludeBundle(path,depends,excludeExtension);
+//                }
+//            }
+//        }
 
-        private void DealWithAtlas()
-        {
-            List<string> atlasPaths = (from path in bundleAssetPathList
-                                       where Path.GetExtension(path).ToLower() == ".spriteatlas"
-                                       select path).ToList();
-            atlasPaths.ForEach((atlasPath) =>
-            {
-                progressCallback?.Invoke(atlasPath, findIndex / (float)bundleAssetPathList.Count);
-                findIndex++;
+//        private void DealWithAtlas()
+//        {
+//            List<string> atlasPaths = (from path in bundleAssetPathList
+//                                       where Path.GetExtension(path).ToLower() == ".spriteatlas"
+//                                       select path).ToList();
+//            atlasPaths.ForEach((atlasPath) =>
+//            {
+//                progressCallback?.Invoke(atlasPath, findIndex / (float)bundleAssetPathList.Count);
+//                findIndex++;
 
-                SpriteAtlas atlas = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(atlasPath);
-                if (atlas == null)
-                {
-                    Debug.LogError("AssetDependFinder::Find->atlas is null. path = " + atlasPath);
-                }
-                else
-                {
-                    string[] spriteInAtlas = SpriteAtlasUtil.GetDependAssets(atlas);
+//                SpriteAtlas atlas = AssetDatabase.LoadAssetAtPath<SpriteAtlas>(atlasPath);
+//                if (atlas == null)
+//                {
+//                    Debug.LogError("AssetDependFinder::Find->atlas is null. path = " + atlasPath);
+//                }
+//                else
+//                {
+//                    string[] spriteInAtlas = SpriteAtlasUtil.GetDependAssets(atlas);
 
-                    bundleDependAssetDic.Add(atlasPath, new List<string>(spriteInAtlas));
+//                    bundleDependAssetDic.Add(atlasPath, new List<string>(spriteInAtlas));
 
-                    foreach (var spritePath in spriteInAtlas)
-                    {
-                        spriteInAtlasDic.Add(spritePath, atlasPath);
-                    }
-                }
-            });
-        }
+//                    foreach (var spritePath in spriteInAtlas)
+//                    {
+//                        spriteInAtlasDic.Add(spritePath, atlasPath);
+//                    }
+//                }
+//            });
+//        }
         
-    }
-}
+//    }
+//}
--- Scripts/SharpScripts/Editor/DotEditor/Core/BundleDepend/AssetDependTreeView.cs
+++ Scripts/SharpScripts/Editor/DotEditor/Core/BundleDepend/AssetDependTreeView.cs
@@ -1,118 +1,118 @@
-﻿using DotEditor.Core.EGUI.TreeGUI;
-using DotEditor.Util;
-using UnityEditor;
-using UnityEditor.IMGUI.Controls;
-using UnityEngine;
+﻿//using DotEditor.Core.EGUI.TreeGUI;
+//using DotEditor.Util;
+//using UnityEditor;
+//using UnityEditor.IMGUI.Controls;
+//using UnityEngine;
 
-namespace DotEditor.Core.BundleDepend
-{
-    public class AssetDependTreeView : TreeViewWithTreeModel<TreeElementWithData<DependTreeData>>
-    {
-        private GUIContent warningIconContent;
-        private int curMaxID = 1;
-        public int NextID
-        {
-            get
-            {
-                return curMaxID++;
-            }
-        }
+//namespace DotEditor.Core.BundleDepend
+//{
+//    public class AssetDependTreeView : TreeViewWithTreeModel<TreeElementWithData<DependTreeData>>
+//    {
+//        private GUIContent warningIconContent;
+//        private int curMaxID = 1;
+//        public int NextID
+//        {
+//            get
+//            {
+//                return curMaxID++;
+//            }
+//        }
 
-        internal AssetDependWindow dependWin = null;
+//        internal AssetDependWindow dependWin = null;
 
-        public AssetDependTreeView(TreeViewState state, TreeModel<TreeElementWithData<DependTreeData>> model) :
-            base(state, model)
-        {
-            warningIconContent = EditorGUIUtility.IconContent("console.warnicon.sml", "Repeat");
-            showBorder = true;
-            showAlternatingRowBackgrounds = true;
-            rowHeight = EditorGUIUtility.singleLineHeight*2;
-            Reload();
-        }
+//        public AssetDependTreeView(TreeViewState state, TreeModel<TreeElementWithData<DependTreeData>> model) :
+//            base(state, model)
+//        {
+//            warningIconContent = EditorGUIUtility.IconContent("console.warnicon.sml", "Repeat");
+//            showBorder = true;
+//            showAlternatingRowBackgrounds = true;
+//            rowHeight = EditorGUIUtility.singleLineHeight*2;
+//            Reload();
+//        }
 
-        //private List<int> cachedExpandIDs = new List<int>();
-        //protected override void ExpandedStateChanged()
-        //{
-        //    List<int> ids = new List<int>(GetExpanded());
+//        //private List<int> cachedExpandIDs = new List<int>();
+//        //protected override void ExpandedStateChanged()
+//        //{
+//        //    List<int> ids = new List<int>(GetExpanded());
 
-        //    IEnumerable<int> collapseIDs = cachedExpandIDs.Except(ids);
-        //    foreach(var id in collapseIDs)
-        //    {
-        //        TreeElementWithData<AssetData> treeViewData = treeModel.Find(id);
-        //        if (treeViewData.Data.dependAssets.Count > 0)
-        //        {
-        //            treeViewData.children?.Clear();
-        //            TreeElementWithData<AssetData> dependTreeData = new TreeElementWithData<AssetData>(null, "", treeViewData.depth + 1, NextID);
-        //            treeModel.AddElement(dependTreeData, treeViewData, treeViewData.hasChildren ? treeViewData.children.Count : 0);
-        //        }
-        //    }
+//        //    IEnumerable<int> collapseIDs = cachedExpandIDs.Except(ids);
+//        //    foreach(var id in collapseIDs)
+//        //    {
+//        //        TreeElementWithData<AssetData> treeViewData = treeModel.Find(id);
+//        //        if (treeViewData.Data.dependAssets.Count > 0)
+//        //        {
+//        //            treeViewData.children?.Clear();
+//        //            TreeElementWithData<AssetData> dependTreeData = new TreeElementWithData<AssetData>(null, "", treeViewData.depth + 1, NextID);
+//        //            treeModel.AddElement(dependTreeData, treeViewData, treeViewData.hasChildren ? treeViewData.children.Count : 0);
+//        //        }
+//        //    }
 
-        //    IEnumerable<int> expandIDs = ids.Except(cachedExpandIDs);
-        //    foreach (var id in expandIDs)
-        //    {
-        //        TreeElementWithData<AssetData> treeViewData = treeModel.Find(id);
-        //        if (treeViewData.Data.dependAssets.Count > 0)
-        //        {
-        //            treeViewData.children?.Clear();
+//        //    IEnumerable<int> expandIDs = ids.Except(cachedExpandIDs);
+//        //    foreach (var id in expandIDs)
+//        //    {
+//        //        TreeElementWithData<AssetData> treeViewData = treeModel.Find(id);
+//        //        if (treeViewData.Data.dependAssets.Count > 0)
+//        //        {
+//        //            treeViewData.children?.Clear();
 
-        //            for (int j = 0; j < treeViewData.Data.dependAssets.Count; ++j)
-        //            {
-        //                AssetData aData = treeViewData.Data.dependAssets[j];
-        //                TreeElementWithData<AssetData> dependTreeData = new TreeElementWithData<AssetData>(aData, "", treeViewData.depth + 1, NextID);
-        //                treeModel.AddElement(dependTreeData, treeViewData, treeViewData.hasChildren ? treeViewData.children.Count : 0);
+//        //            for (int j = 0; j < treeViewData.Data.dependAssets.Count; ++j)
+//        //            {
+//        //                AssetData aData = treeViewData.Data.dependAssets[j];
+//        //                TreeElementWithData<AssetData> dependTreeData = new TreeElementWithData<AssetData>(aData, "", treeViewData.depth + 1, NextID);
+//        //                treeModel.AddElement(dependTreeData, treeViewData, treeViewData.hasChildren ? treeViewData.children.Count : 0);
 
-        //                if(aData.dependAssets.Count>0)
-        //                {
-        //                    TreeElementWithData<AssetData> emptyData = new TreeElementWithData<AssetData>(null, "", treeViewData.depth + 2, NextID);
-        //                    treeModel.AddElement(emptyData, dependTreeData, dependTreeData.hasChildren ? dependTreeData.children.Count : 0);
-        //                }
-        //            }
-        //        }
-        //    }
+//        //                if(aData.dependAssets.Count>0)
+//        //                {
+//        //                    TreeElementWithData<AssetData> emptyData = new TreeElementWithData<AssetData>(null, "", treeViewData.depth + 2, NextID);
+//        //                    treeModel.AddElement(emptyData, dependTreeData, dependTreeData.hasChildren ? dependTreeData.children.Count : 0);
+//        //                }
+//        //            }
+//        //        }
+//        //    }
 
-        //    cachedExpandIDs = ids;
-        //}
+//        //    cachedExpandIDs = ids;
+//        //}
         
-        protected override bool CanMultiSelect(TreeViewItem item)
-        {
-            return false;
-        }
+//        protected override bool CanMultiSelect(TreeViewItem item)
+//        {
+//            return false;
+//        }
 
-        protected override bool CanStartDrag(CanStartDragArgs args)
-        {
-            return false;
-        }
+//        protected override bool CanStartDrag(CanStartDragArgs args)
+//        {
+//            return false;
+//        }
         
-        protected override void RowGUI(RowGUIArgs args)
-        {
-            var item = (TreeViewItem<TreeElementWithData<DependTreeData>>)args.item;
-            DependTreeData assetData = item.data.Data;
+//        protected override void RowGUI(RowGUIArgs args)
+//        {
+//            var item = (TreeViewItem<TreeElementWithData<DependTreeData>>)args.item;
+//            DependTreeData assetData = item.data.Data;
 
-            if (assetData == null)
-            {
-                return;
-            }
-            Rect contentRect = args.rowRect;
-            contentRect.x += GetContentIndent(item);
-            contentRect.width -= GetContentIndent(item);
+//            if (assetData == null)
+//            {
+//                return;
+//            }
+//            Rect contentRect = args.rowRect;
+//            contentRect.x += GetContentIndent(item);
+//            contentRect.width -= GetContentIndent(item);
 
-            Rect rect = contentRect;
-            rect.width -= 80;
+//            Rect rect = contentRect;
+//            rect.width -= 80;
             
-            if(assetData.isBundle)
-            {
-                EditorGUI.LabelField(rect, assetData.assetPath);
-            }else
-            {
-                EditorGUI.LabelField(rect, assetData.assetPath+$"({assetData.repeatCount})");
-            }
+//            if(assetData.isBundle)
+//            {
+//                EditorGUI.LabelField(rect, assetData.assetPath);
+//            }else
+//            {
+//                EditorGUI.LabelField(rect, assetData.assetPath+$"({assetData.repeatCount})");
+//            }
 
-            rect.x += rect.width+5;
-            rect.width = 70;
-            if(GUI.Button(rect,"selected"))
-            {
-                SelectionUtil.ActiveObject(assetData.assetPath);
-            }
-        }
-    }
-}
+//            rect.x += rect.width+5;
+//            rect.width = 70;
+//            if(GUI.Button(rect,"selected"))
+//            {
+//                SelectionUtil.ActiveObject(assetData.assetPath);
+//            }
+//        }
+//    }
+//}
