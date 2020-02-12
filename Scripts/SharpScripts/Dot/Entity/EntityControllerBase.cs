0b90d767c599ea4efa94c017786966b6e88327d5
li.yubin <liyubin724@163.com>
2020-02-13 01:49:29 +0800

    准备重构Entity的实现

---
+++ Scripts/SharpScripts/Dot/Entity/EntityControllerBase.cs
@@ -0,0 +1,197 @@
+﻿using Dot.Dispatch;
+using Dot.Log;
+using Dot.Lua;
+using System;
+using XLua;
+using EventHandler = Dot.Dispatch.EventHandler;
+
+namespace Dot.Entity
+{
+    [Flags]
+    public enum EntityControllerType
+    {
+        VirtualView = 1<<0,
+        Avatar = 1<<1,
+        Move = 1<<2,
+        Animator = 1<<3,
+    }
+
+    public abstract class EntityControllerBase
+    {
+        internal static readonly string LOGGER_NAME = "EntityController";
+        private static readonly string LUA_CONTROLLER_NAME = "controller";
+
+        public bool Enable { get; set; } = true;
+
+        #region
+        private EntityObject entityObj = null;
+        protected EntityObject Entity { get => entityObj; }
+        public void InitController(EntityObject entity)
+        {
+            entityObj = entity;
+
+            DoInit();
+            RegisterEvents();
+        }
+
+        public void ResetController()
+        {
+            UnregisterEvents();
+            DoReset();
+            eventDispatcher = null;
+            entityObj = null;
+        }
+
+        protected abstract void DoInit();
+
+        protected internal virtual void DoUpdate(float deltaTime) { }
+
+        protected abstract void DoReset();
+
+        #endregion
+
+        #region Operation for Lua
+        private LuaTable objTable = null;
+        protected LuaTable ObjTable
+        {
+            get => objTable;
+        }
+
+        public void BindLuaScript(string luaScript)
+        {
+            if(string.IsNullOrEmpty(luaScript))
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityControllerBase::BindLuaScript->the name of script is empty");
+                return;
+            }
+            int index = luaScript.LastIndexOf("/");
+            string scriptFileName = luaScript;
+            if(index>0)
+            {
+                scriptFileName = luaScript.Substring(index + 1);
+            }
+            if(string.IsNullOrEmpty(scriptFileName))
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityControllerBase::BindLuaScript->the name of script is empty");
+                return;
+            }
+            LuaEnv luaEnv = LuaManager.GetInstance().LuaEnv;
+            LuaTable classTable = luaEnv.Global.Get<LuaTable>(scriptFileName);
+            if (classTable == null)
+            {
+                luaEnv.DoString(string.Format("require (\"{0}\")", luaScript));
+                classTable = luaEnv.Global.Get<LuaTable>(scriptFileName);
+                if(classTable == null)
+                {
+                    LogUtil.LogError(LOGGER_NAME, $"EntityControllerBase::BindLuaScript->Require lua Script failed.luaScript = {luaScript}");
+                    return;
+                }
+            }
+
+            LuaFunction callFun = classTable.Get<LuaFunction>("__call");
+            objTable = callFun.Func<LuaTable, LuaTable>(classTable);
+
+            objTable.Set(LUA_CONTROLLER_NAME, this);
+            OnLuaBinded();
+
+            callFun.Dispose();
+            classTable.Dispose();
+        }
+
+        protected virtual void OnLuaBinded()
+        {
+        }
+
+        protected void CallAction(string funcName)
+        {
+            if(objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallAction->objTable is null");
+                return;
+            }
+
+            objTable.Get<Action<LuaTable>>(funcName)?.Invoke(objTable);
+        }
+
+        protected void CallActoin<T>(string funcName,T param1)
+        {
+            if (objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallAction->objTable is null");
+                return;
+            }
+
+            objTable.Get<Action<LuaTable,T>>(funcName)?.Invoke(objTable,param1);
+        }
+
+        protected void CallAction<T,K>(string funcName,T param1,K param2)
+        {
+            if (objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallAction->objTable is null");
+                return;
+            }
+
+            objTable.Get<Action<LuaTable, T,K>>(funcName)?.Invoke(objTable, param1,param2);
+        }
+
+        protected R CallFunc<R>(string funcName)
+        {
+            if (objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallFunc->objTable is null");
+                return default(R);
+            }
+
+            Func<LuaTable, R> result = objTable.Get<Func<LuaTable, R>>(funcName);
+            if(result !=null)
+            {
+                return result(objTable);
+            }
+            return default(R);
+        }
+
+        protected R CallFunc<T,R>(string funcName,T param1)
+        {
+            if (objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallFunc->objTable is null");
+                return default(R);
+            }
+
+            Func<LuaTable, T,R> result = objTable.Get<Func<LuaTable, T,R>>(funcName);
+            if (result != null)
+            {
+                return result(objTable,param1);
+            }
+            return default(R);
+        }
+
+        protected R CallFunc<T, K,R>(string funcName, T param1, K param2)
+        {
+            if (objTable == null)
+            {
+                LogUtil.LogError(LOGGER_NAME, "EntityContrllerBase::CallFunc->objTable is null");
+                return default(R);
+            }
+
+            Func<LuaTable, T, K,R> result = objTable.Get<Func<LuaTable, T,K, R>>(funcName);
+            if (result != null)
+            {
+                return result(objTable, param1,param2);
+            }
+            return default(R);
+        }
+        #endregion
+
+        #region Operation for event
+        private EventDispatcher eventDispatcher = null;
+        protected void SendEvent(int eventID, params object[] values) => eventDispatcher.TriggerEvent(eventID, 0, values);
+        protected void RegisterEvent(int eventID, EventHandler handler) => eventDispatcher.RegisterEvent(eventID, handler);
+        protected void UnregisterEvent(int eventID, EventHandler handler) => eventDispatcher.UnregisterEvent(eventID, handler);
+
+        protected virtual void RegisterEvents() { }
+        protected virtual void UnregisterEvents() { }
+        #endregion
+    }
+}
---
+++ Scripts/SharpScripts/Dot/Entity/EntityObject.cs
@@ -0,0 +1,105 @@
+﻿using Dot.Dispatch;
+using Dot.Log;
+using System.Collections.Generic;
+using EventHandler = Dot.Dispatch.EventHandler;
+
+namespace Dot.Entity
+{
+    public class EntityObject
+    {
+        private static readonly string LOGGER_NAME = "EntityObject";
+
+        public long UniqueID { get; set; }
+        public int Category { get; set; }
+        public string Name { get; set; }
+
+        public EntityObject()
+        {
+            eventDispatcher = new EventDispatcher();
+        }
+
+        public void DoUpdate(float deltaTime)
+        {
+            foreach(var kvp in controllerDic)
+            {
+                if(kvp.Value.Enable)
+                {
+                    kvp.Value.DoUpdate(deltaTime);
+                }
+            }
+        }
+
+        public void DoReset()
+        {
+            foreach(var kvp in controllerDic)
+            {
+                kvp.Value.ResetController();
+            }
+            RemoveAllController();
+        }
+
+        #region operation for event
+        private EventDispatcher eventDispatcher = null;
+        public void SendEvent(int eventID, params object[] values) => eventDispatcher.TriggerEvent(eventID, 0, values);
+        public void RegisterEvent(int eventID, EventHandler handler) => eventDispatcher.RegisterEvent(eventID, handler);
+        public void UnregisterEvent(int eventID, EventHandler handler) => eventDispatcher.UnregisterEvent(eventID, handler);
+        #endregion
+
+        #region operation for controller
+        private Dictionary<EntityControllerType, EntityControllerBase> controllerDic = new Dictionary<EntityControllerType, EntityControllerBase>();
+        public T GetController<T>(EntityControllerType controllerType) where T:EntityControllerBase
+        {
+            if(controllerDic.TryGetValue(controllerType,out EntityControllerBase controller))
+            {
+                return (T)controller;
+            }
+            return null;
+        }
+
+        public bool HasController(EntityControllerType controllerType)
+        {
+            return controllerDic.ContainsKey(controllerType);
+        }
+
+        public void AddController(EntityControllerType controllerType, EntityControllerBase controller)
+        {
+            if (controller == null)
+            {
+                LogUtil.LogError(typeof(EntityObject), "EntityObject::this[index]-> value is null");
+                return;
+            }
+            if (!controllerDic.ContainsKey(controllerType))
+            {
+                controllerDic.Add(controllerType, controller);
+            }
+            else
+            {
+                LogUtil.LogError(typeof(EntityObject), "EntityObject::this[index]->controller has been added.if you want to replace it,please use ReplaceController instead");
+            }
+        }
+
+        public void ReplaceController(EntityControllerType controllerType, EntityControllerBase controller)
+        {
+            RemoveController(controllerType);
+            AddController(controllerType, controller);
+        }
+
+        public void RemoveController(EntityControllerType controllerType)
+        {
+            if(HasController(controllerType))
+            {
+                controllerDic.Remove(controllerType);
+            }
+        }
+
+        public void RemoveAllController()
+        {
+            foreach(var kvp in controllerDic)
+            {
+                kvp.Value.ResetController();
+            }
+            controllerDic.Clear();
+        }
+        #endregion
+    }
+}
