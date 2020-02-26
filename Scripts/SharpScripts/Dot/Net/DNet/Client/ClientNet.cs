b2bf33a02f1749e5ba8b22b612c24ecad28bcaf5
li.yubin <liyubin724@163.com>
2020-02-27 03:41:23 +0800

    添加网络相关的内容

---
+++ Scripts/SharpScripts/Dot/Net/DNet/Client/ClientNet.cs
@@ -0,0 +1,24 @@
+﻿using Dot.Net.Message;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dot.Net.Client
+{
+    public class ClientNet 
+    {
+        private IMessageWriter messageWriter = null;
+        private IClientNetDataReceiver dataReceiver = null;
+
+        private ClientNetSession netSession = null;
+
+        public ClientNet(IMessageWriter writer,IClientNetDataReceiver receiver)
+        {
+            messageWriter = writer;
+            dataReceiver = receiver;
+        }
+
+
+
+    }
+}
