4568cd42d90e41443c3ce68da6d4cfdc3f6fb19e
li.yubin <liyubin724@163.com>
2020-03-07 11:40:19 +0800

    添加Editor中Resources操作

---
+++ Scripts/SharpScripts/Editor/DotEditor/EGUI/DotEditorResorces.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace DotEditor.EGUI
+{
+    public static class DotEditorResorces
+    {
+        public static Texture2D GenerateGridTexture(Color line, Color bg)
+        {
+            Texture2D tex = new Texture2D(64, 64);
+            Color[] cols = new Color[64 * 64];
+            for (int y = 0; y < 64; y++)
+            {
+                for (int x = 0; x < 64; x++)
+                {
+                    Color col = bg;
+                    if (y % 16 == 0 || x % 16 == 0) col = Color.Lerp(line, bg, 0.65f);
+                    if (y == 63 || x == 63) col = Color.Lerp(line, bg, 0.35f);
+                    cols[(y * 64) + x] = col;
+                }
+            }
+            tex.SetPixels(cols);
+            tex.wrapMode = TextureWrapMode.Repeat;
+            tex.filterMode = FilterMode.Bilinear;
+            tex.name = "Grid";
+            tex.Apply();
+            return tex;
+        }
+
+        public static Texture2D GenerateCrossTexture(Color line)
+        {
+            Texture2D tex = new Texture2D(64, 64);
+            Color[] cols = new Color[64 * 64];
+            for (int y = 0; y < 64; y++)
+            {
+                for (int x = 0; x < 64; x++)
+                {
+                    Color col = line;
+                    if (y != 31 && x != 31) col.a = 0;
+                    cols[(y * 64) + x] = col;
+                }
+            }
+            tex.SetPixels(cols);
+            tex.wrapMode = TextureWrapMode.Clamp;
+            tex.filterMode = FilterMode.Bilinear;
+            tex.name = "Grid";
+            tex.Apply();
+            return tex;
+        }
+    }
+}
