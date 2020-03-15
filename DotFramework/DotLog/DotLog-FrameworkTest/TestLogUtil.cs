8aa56e1a313e7ae69d96696223a24577beb7f7fb
li.yubin <liyubin724@163.com>
2020-03-15 23:55:25 +0800

    添加单元测试代码

---
+++ DotFramework/DotLog/DotLog-FrameworkTest/TestLogUtil.cs
@@ -0,0 +1,30 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using Dot.Log;
+using NUnit.Framework;
+using System.IO;
+
+namespace DotLog_FrameworkTest
+{
+    [TestFixture]
+    public class TestLogUtil
+    {
+        private static readonly string CONFIG_PATH = "./DotLog-FrameworkTest/log_config.xml";
+
+        [OneTimeSetUp]
+        public void TestSetUp()
+        {
+            LogUtil.Initalize(File.ReadAllText(CONFIG_PATH), LogLevelType.Info);
+        }
+
+
+
+        [Test]
+        public void TestLogError()
+        {
+            LogUtil.LogError("TestTag", "Just for Test");
+            LogUtil.LogError("TestTag", "Just for Test");
+            LogUtil.LogError("TestTag", "Just for Test");
+        }
+    }
+}
