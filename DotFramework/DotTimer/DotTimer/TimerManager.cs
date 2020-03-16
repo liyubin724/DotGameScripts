67035f3ab6ffa15f830595986bd31b5718040e7b
li.yubin <liyubin724@163.com>
2020-03-16 15:27:32 +0800

    添加定时器实现逻辑

---
+++ DotFramework/DotTimer/DotTimer/TimerManager.cs
@@ -0,0 +1,88 @@
+﻿using Dot.Core;
+using Dot.Core.Proxy;
+
+namespace Dot.Timer
+{
+    public delegate void TimerCallback(object obj);
+
+    public class TimerManager : Singleton<TimerManager>
+    {
+        private HierarchicalTimerWheel hTimerWheel = null;
+        private bool isPause = false;
+
+        protected override void DoInit()
+        {
+            hTimerWheel = new HierarchicalTimerWheel();
+            UpdateProxy.GetInstance().DoUpdateHandle += DoUpdate;
+        }
+
+        private void DoUpdate(float deltaTime)
+        {
+            if (!isPause && hTimerWheel != null)
+            {
+                hTimerWheel.OnUpdate(deltaTime);
+            }
+        }
+
+        public void Pause()
+        {
+            isPause = true;
+        }
+
+        public void Resume()
+        {
+            isPause = false;
+        }
+
+        public TimerTaskInfo AddTimer(float intervalInSec,
+                                                float totalInSec,
+                                                TimerCallback startCallback,
+                                                TimerCallback intervalCallback,
+                                                TimerCallback endCallback,
+                                                object callbackData)
+        {
+            if (hTimerWheel == null) return null;
+
+            TimerTask task = hTimerWheel.GetIdleTimerTask();
+            task.OnReused(intervalInSec, totalInSec, startCallback, intervalCallback, endCallback, callbackData);
+            return hTimerWheel.AddTimerTask(task);
+        }
+
+        public TimerTaskInfo AddTimer(float intervalInSec,
+                                                float totalInSec,
+                                                TimerCallback intervalCallback,
+                                                TimerCallback endCallback,
+                                                object callbackData = null)
+        {
+            return AddTimer(intervalInSec, totalInSec, null, intervalCallback, endCallback, callbackData);
+        }
+
+        public TimerTaskInfo AddIntervalTimer(float intervalInSec, TimerCallback intervalCallback, object callbackData = null)
+        {
+            return AddTimer(intervalInSec, 0f, null, intervalCallback, null, callbackData);
+        }
+
+        public TimerTaskInfo AddEndTimer(float totalInSec, TimerCallback endCallback,object callbackData = null)
+        {
+            return AddTimer(totalInSec, totalInSec, null, null, endCallback, callbackData);
+        }
+
+        public bool RemoveTimer(TimerTaskInfo taskInfo)
+        {
+            if (hTimerWheel != null)
+            {
+                return hTimerWheel.RemoveTimerTask(taskInfo);
+            }
+            return false;
+        }
+
+        public override void DoDispose()
+        {
+            UpdateProxy.GetInstance().DoUpdateHandle -= DoUpdate;
+
+            hTimerWheel?.Clear();
+            isPause = false;
+            hTimerWheel = null;
+        }
+    }
+}
