b6556b21e3cbde08e7e0332af67428fc80beff29
li.yubin <liyubin724@163.com>
2020-03-22 02:31:14 +0800

    添加网络使用PB的DEMO

--- DotFramework/DotNet/DotNetDemo/DotNetTest.cs
+++ DotFramework/DotNet/DotNetDemo/DotNetTest.cs
@@ -1,23 +1,17 @@
 ﻿//This class is used to test the netframework
 
-/*
 using Dot.Core.Proxy;
 using Dot.Log;
 using Dot.Net.Client;
 using Dot.Net.Server;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Game.Net.Proto;
+using Google.Protobuf;
 
 namespace Dot.Net.Demo
 {
-    [Serializable]
-    public class NetMessage
-    {
-        public string message;
-    }
-
     public class DotNetTest : MonoBehaviour
     {
         public TextAsset log4netConfig = null;
@@ -36,16 +30,15 @@ namespace Dot.Net.Demo
         {
             UpdateProxy.GetInstance().DoLateUpdate();
         }
+
         private ServerNetListener serverNet = null;
         private List<string> serverReceivedList = new List<string>();
 
-        private ClientNet clientNet = null;
-        private string clientInputStr = string.Empty;
-        private List<string> clientReceivedList = new List<string>();
-
         private ClientNet clientNet1 = null;
-        private string clientInputStr1 = string.Empty;
         private List<string> clientReceivedList1 = new List<string>();
+
+        private ClientNet clientNet2 = null;
+        private List<string> clientReceivedList2 = new List<string>();
 
         private int index = 0;
         private void OnGUI()
@@ -56,26 +49,25 @@ namespace Dot.Net.Demo
                 {
                     if (GUILayout.Button("Start Server"))
                     {
-                        serverNet = NetManager.GetInstance().StartupAsServer(1100);
-                        serverNet.RegisterHandler(1000, (netID, messageID, message) =>
+                        serverNet = NetManager.GetInstance().CreateServerNet(1100);
+                        C2SProto_Parser.RegisterParser(serverNet);
+                        serverNet.RegisterMessageHandler(C2SProto.C2S_LOGIN, (netID,messageID, message) =>
                         {
-                            Debug.Log("message = " + ((string)message));
-                            try
-                            {
-                                NetMessage netMess = JsonConvert.DeserializeObject<NetMessage>((string)message);
-                                serverReceivedList.Add(netMess.message);
-                            }
-                            catch
-                            {
+                            LoginRequest request = (LoginRequest)message;
+                            serverReceivedList.Add($"netID = {netID},account = {request.UserAccount},password={request.Password}");
 
-                            }
+                            LoginResponse response = new LoginResponse();
+                            response.Result = true;
+                            serverNet.SendData(netID, S2CProto.S2C_LOGIN, response.ToByteArray());
+                        });
+                        serverNet.RegisterMessageHandler(C2SProto.C2S_SHOP_LIST, (netID, messageID, message) =>
+                        {
+                            ShopListRequest request = (ShopListRequest)message;
+                            serverReceivedList.Add($"netID = {netID},ShopType = {request.ShopType},PageNumber={request.PageNumber}");
 
-                            NetMessage nm = new NetMessage()
-                            {
-                                message = "--->" + index.ToString()
-                            };
-                            index++;
-                            serverNet.SendMessage<NetMessage>(netID, 10001, nm);
+                            ShopListResponse response = new ShopListResponse();
+                            response.Names = $"Test Shop ->{request.ShopType} ->{request.PageNumber}";
+                            serverNet.SendData(netID, S2CProto.S2C_SHOP_LIST, response.ToByteArray());
                         });
                     }
                     GUILayout.Label("Received:");
@@ -87,25 +79,38 @@ namespace Dot.Net.Demo
                 {
                     if (GUILayout.Button("Start Client"))
                     {
-                        clientNet = NetManager.GetInstance().CreateClientNet(0);
-                        clientNet.Connect("127.0.0.1", 1100);
-                        clientNet.RegisterHandler(10001, (message) =>
+                        clientNet1 = NetManager.GetInstance().CreateClientNet();
+                        S2CProto_Parser.RegisterParser(clientNet1);
+
+                        clientNet1.Connect("127.0.0.1", 1100);
+                        clientNet1.RegisterMessageHandler(S2CProto.S2C_LOGIN, (messageID,message) =>
                         {
-                            NetMessage netMess = JsonConvert.DeserializeObject<NetMessage>((string)message);
-                            clientReceivedList.Add(netMess.message);
+                            LoginResponse response = (LoginResponse)message;
+                            clientReceivedList1.Add($"netID = {clientNet1.UniqueID},Result={response.Result}");
+                        });
+                        clientNet1.RegisterMessageHandler(S2CProto.S2C_SHOP_LIST, (messageID, message) =>
+                        {
+                            ShopListResponse response = (ShopListResponse)message;
+                            clientReceivedList1.Add($"netID = {clientNet1.UniqueID},names={response.Names}");
                         });
                     }
-                    GUILayout.Label("Input:");
-                    clientInputStr = GUILayout.TextArea(clientInputStr);
-                    if (GUILayout.Button("Send"))
+
+                    if(GUILayout.Button("Login"))
                     {
-                        clientNet.SendMessage<NetMessage>(1000, new NetMessage()
-                        {
-                            message = clientInputStr
-                        });
+                        LoginRequest request = new LoginRequest();
+                        request.UserAccount = 123456;
+                        request.Password = "TTF001";
+                        clientNet1.SendPBMessage<LoginRequest>(C2SProto.C2S_LOGIN,request);
+                    }
+                    if(GUILayout.Button("Get Shop List"))
+                    {
+                        ShopListRequest request = new ShopListRequest();
+                        request.ShopType = 1;
+                        request.PageNumber = 10;
+                        clientNet1.SendPBMessage<ShopListRequest>(C2SProto.C2S_SHOP_LIST, request);
                     }
                     GUILayout.Label("Received:");
-                    GUILayout.TextArea(string.Join("\r\n", clientReceivedList.ToArray()));
+                    GUILayout.TextArea(string.Join("\r\n", clientReceivedList1.ToArray()));
 
                 }
                 GUILayout.EndVertical();
@@ -114,26 +119,50 @@ namespace Dot.Net.Demo
                 {
                     if (GUILayout.Button("Start Client"))
                     {
-                        clientNet1 = NetManager.GetInstance().CreateClientNet(1);
-                        clientNet1.Connect("127.0.0.1", 1100);
-                        clientNet1.RegisterHandler(10001, (message) =>
+                        clientNet2 = NetManager.GetInstance().CreateClientNet();
+                        S2CProto_Parser.RegisterParser(clientNet2);
+
+                        clientNet2.Connect("127.0.0.1", 1100);
+                        clientNet2.RegisterMessageHandler(S2CProto.S2C_LOGIN, (messageID, message) =>
                         {
-                            NetMessage netMess = JsonConvert.DeserializeObject<NetMessage>((string)message);
-                            clientReceivedList1.Add(netMess.message);
+                            LoginResponse response = (LoginResponse)message;
+                            clientReceivedList2.Add($"netID = {clientNet2.UniqueID},Result={response.Result}");
+                        });
+                        clientNet2.RegisterMessageHandler(S2CProto.S2C_SHOP_LIST, (messageID, message) =>
+                        {
+                            ShopListResponse response = (ShopListResponse)message;
+                            clientReceivedList2.Add($"netID = {clientNet2.UniqueID},names={response.Names}");
                         });
                     }
-                    GUILayout.Label("Input:");
-                    clientInputStr1 = GUILayout.TextArea(clientInputStr1);
-                    if (GUILayout.Button("Send"))
+
+                    if (GUILayout.Button("Login"))
                     {
-                        clientNet1.SendMessage<NetMessage>(1000, new NetMessage()
-                        {
-                            message = clientInputStr1
-                        });
+                        LoginRequest request = new LoginRequest();
+                        request.UserAccount = 654321;
+                        request.Password = "DDR001";
+                        clientNet2.SendPBMessage<LoginRequest>(C2SProto.C2S_LOGIN, request);
+                    }
+                    if (GUILayout.Button("Get Shop List"))
+                    {
+                        ShopListRequest request = new ShopListRequest();
+                        request.ShopType = 2;
+                        request.PageNumber = 11;
+                        clientNet2.SendPBMessage<ShopListRequest>(C2SProto.C2S_SHOP_LIST, request);
                     }
                     GUILayout.Label("Received:");
-                    GUILayout.TextArea(string.Join("\r\n", clientReceivedList1.ToArray()));
+                    GUILayout.TextArea(string.Join("\r\n", clientReceivedList2.ToArray()));
 
+                }
+                GUILayout.EndVertical();
+
+                GUILayout.BeginVertical();
+                {
+                    if(GUILayout.Button("Clear Log"))
+                    {
+                        clientReceivedList1.Clear();
+                        clientReceivedList2.Clear();
+                        serverReceivedList.Clear();
+                    }
                 }
                 GUILayout.EndVertical();
             }
@@ -145,4 +174,3 @@ namespace Dot.Net.Demo
     }
 
 }
-*/
