a2f294db41e653b2486bbd882317847bba612c26
li.yubin <liyubin724@163.com>
2020-03-22 20:17:44 +0800

    开始添加编辑器相关的内容

---
+++ DotFramework/DotAsset/DotAssetEditor/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotAssetEditor
+{
+    public class Class1
+    {
+    }
+}
---
+++ DotFramework/DotCore/DotCoreEditor/Util/PrefabUtil.cs
@@ -0,0 +1,82 @@
+﻿using ReflectionMagic;
+using System;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+namespace DotEditor.Core.Util
+{
+    public static class PrefabUtil
+    {
+        public static bool IsPrefab(string assetPath)
+        {
+            if (!string.IsNullOrEmpty(assetPath) && assetPath.EndsWith(".prefab", StringComparison.OrdinalIgnoreCase))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool IsMissingNestPrefab(string assetPath)
+        {
+            if(!IsPrefab(assetPath))
+            {
+                return false;
+            }
+
+            OpenPrefabStage(assetPath);
+
+            PrefabStage stage = PrefabStageUtility.GetCurrentPrefabStage();
+            GameObject rootGO = stage.prefabContentsRoot;
+            Transform[] transforms = rootGO.GetComponentsInChildren<Transform>();
+            foreach (var t in transforms)
+            {
+                if (t.name.IndexOf("Missing Prefab") >= 0)
+                {
+                    return true;
+                }
+            }
+
+            ClosePrefabStage();
+
+            return false;
+        }
+
+        public static bool IsInPrefabStage()
+        {
+            var stage = UnityEditor.Experimental.SceneManagement.PrefabStageUtility.GetCurrentPrefabStage();
+            return stage != null;
+        }
+
+        public static PrefabStage OpenPrefabStage(string prefabPath)
+        {
+            if(IsPrefab(prefabPath))
+            {
+                GameObject prefabGO = AssetDatabase.LoadAssetAtPath<GameObject>(prefabPath);
+                if(prefabGO!=null)
+                {
+                    dynamic utilityDynamic = typeof(PrefabStageUtility).AsDynamicType();
+                    utilityDynamic.OpenPrefab(prefabPath);
+
+                    return PrefabStageUtility.GetCurrentPrefabStage();
+                }
+            }
+
+            return null;
+        }
+
+        public static void ClosePrefabStage()
+        {
+            if(IsInPrefabStage())
+            {
+                Type changeType = AssemblyUtil.GetTypeByFullName("UnityEditor.SceneManagement.StageNavigationManager+Analytics+ChangeType");
+                var changeTypeValue = Enum.Parse(changeType, "NavigateBackViaHierarchyHeaderLeftArrow");
+
+                Type type = AssemblyUtil.GetTypeByFullName("UnityEditor.SceneManagement.StageNavigationManager");
+                dynamic typeDynamic = type.AsDynamicType();
+                var snMgr = typeDynamic.instance;
+                snMgr.NavigateBack(changeTypeValue);
+            }
+        }
+    }
+}
