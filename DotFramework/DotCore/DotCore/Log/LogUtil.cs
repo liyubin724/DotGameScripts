78b62e27aab486af72f160abd5bd96b73afee8ad
li.yubin <liyubin724@163.com>
2020-04-08 02:58:00 +0800

    重新调整Core的层次，未来Core将做为所有库的中间转接库来供其它所有的接口使用

---
+++ DotFramework/DotCore/DotCore/Log/LogUtil.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+
+namespace Dot.Core.Log
+{
+    public static class LogUtil
+    {
+        private static LogLevelType logLevel = LogLevelType.Info;
+        private static ILog logger = null;
+
+        public static void SetLogger(ILog logger,LogLevelType logLevel)
+        {
+            LogUtil.logLevel = logLevel;
+            LogUtil.logger = logger;
+
+            if(logger!=null)
+            {
+                Application.logMessageReceived += OnMessageReceived;
+            }
+        }
+
+        private static void OnMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            if (type == LogType.Exception)
+            {
+                LogFatal("Exception", $"{condition}\r\n{stackTrace}");
+            }
+        }
+
+        #region Verbose
+        public static void LogVerbose(string tagName, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Verbose)
+            {
+                logger.LogVerbose(tagName, msg);
+            }
+        }
+
+        public static void LogDebug(Type tagType, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Verbose)
+            {
+                logger.LogVerbose(tagType, msg);
+            }
+        }
+        #endregion
+
+        #region Info
+        public static void LogInfo(string tagName, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Info)
+            {
+                logger.LogInfo(tagName, msg);
+            }
+        }
+
+        public static void LogInfo(Type tagType, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Info)
+            {
+                logger.LogInfo(tagType, msg);
+            }
+        }
+        #endregion
+
+        #region Warning
+        public static void LogWarning(Type tagType, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Warning)
+            {
+                logger.LogWarning(tagType, msg);
+            }
+        }
+
+        public static void LogWarning(string tagName, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Warning)
+            {
+                logger.LogWarning(tagName, msg);
+            }
+        }
+        #endregion
+
+        #region Error
+        public static void LogError(Type tagType, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Error)
+            {
+                logger.LogError(tagType, msg);
+            }
+        }
+
+        public static void LogError(string tagName, string msg)
+        {
+            if (logger != null && logLevel <= LogLevelType.Error)
+            {
+                logger.LogError(tagName, msg);
+            }
+        }
+        #endregion
+
+        #region Fatal
+        private static void LogFatal(string tagName, string msg)
+        {
+            if (logger != null)
+            {
+                logger.LogFatal(tagName, msg);
+            }
+        }
+        #endregion
+    }
+}
