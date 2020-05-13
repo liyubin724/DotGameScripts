a27084a584e685bf55e208ffa58fc163bd2864fc
yubin.li <liyubin.724@163.com>
2020-05-13 17:17:03 +0800

    准备添加List与Array数组的绘制

--- Dot/DotEditor/NativeDrawer/DefaultTypeDrawer/DefaultListDrawer.cs
+++ Dot/DotEditor/NativeDrawer/DefaultTypeDrawer/DefaultListDrawer.cs
@@ -5,24 +5,47 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditorInternal;
 
 namespace DotEditor.NativeDrawer.DefaultTypeDrawer
 {
     [CustomTypeDrawer(typeof(IList))]
     public class DefaultListDrawer : NativeTypeDrawer
     {
+        private IList list = null;
+        private ReorderableList rList = null;
         public DefaultListDrawer(object target, FieldInfo field) : base(target, field)
         {
+            
         }
 
         protected override bool IsValid()
         {
-            throw new NotImplementedException();
+            return typeof(IList).IsAssignableFrom(ValueType);
         }
 
         protected override void OnDraw(string label)
         {
-            throw new NotImplementedException();
+            if(rList == null)
+            {
+                list = GetValue<IList>();
+                if(list == null)
+                {
+                    Value = Activator.CreateInstance(ValueType);
+                    list = GetValue<IList>();
+                }
+
+                rList = new ReorderableList(list, ValueType.GenericTypeArguments[0], true, true, true, true);
+                rList.drawHeaderCallback = (rect) =>
+                {
+                    EditorGUI.LabelField(rect,Field.Name);
+                };
+                rList.drawElementCallback = (rect, index, isActive, isFocused) =>
+                {
+                    
+                };
+            }
         }
     }
 }
