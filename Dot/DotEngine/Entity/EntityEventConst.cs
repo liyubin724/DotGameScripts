d8c8bb5c61e14e71266f51f501669a9a28308156
li.yubin <liyubin724@163.com>
2020-05-26 23:47:34 +0800

    添加美术资源

--- Dot/DotEngine/Entity/EntityEventConst.cs
+++ Dot/DotEngine/Entity/EntityEventConst.cs
@@ -8,8 +8,15 @@ namespace Dot.Entity
 {
     public static class EntityEventConst
     {
-        public static int ENTITY_POSITION_CHANGED = 1000;
-        public static int ENTITY_ROTATION_CHANGED = 1001;
-        public static int ENTITY_FORWARD_CHANGED = 1002;
+        public static int ENTITY_POSITION_CHANGED = 100;
+        public static int ENTITY_ROTATION_CHANGED = 101;
+        public static int ENTITY_FORWARD_CHANGED = 102;
+
+        public static int ENTITY_SKELETON_LOAED = 200;
+        public static int ENTITY_PART_LOAED = 201;
+        public static int ENTITY_SKELETON_UNLOAD = 202;
+        public static int ENTITY_PART_UNLOAD = 203;
+
+
     }
 }
