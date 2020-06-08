b85dfb36977952fda3783a7c3079611e4de00afa
yubin.li <liyubin724@163.com>
2020-06-08 19:14:45 +0800

    添加协议生成工具

--- DotFramework/Net/PBConfigTool/Program.cs
+++ DotFramework/Net/PBConfigTool/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,25 @@ using System.Threading.Tasks;
 
 namespace PBConfigTool
 {
+    class Options
+    {
+        [Option('i',"input",Required =true,HelpText ="")]
+        public string InputFilePath { get; set; }
+        [Option('o',"output",Required =true,HelpText ="")]
+        public string OutputDirPath { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions);
+        }
+
+        static void RunOptions(Options options)
+        {
+
         }
     }
 }
