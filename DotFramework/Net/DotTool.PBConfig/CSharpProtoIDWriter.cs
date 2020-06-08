3e0080692396bee32358cb6366265ca4a5b45509
yubin.li <liyubin724@163.com>
2020-06-08 19:31:26 +0800

    准备添加ID的输出配置

---
+++ DotFramework/Net/DotTool.PBConfig/CSharpProtoIDWriter.cs
@@ -0,0 +1,14 @@
+﻿using DotTool.PBConfig.Writer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotTool.PBConfig
+{
+    public class CSharpProtoIDWriter : IProtoIDWriter
+    {
+
+    }
+}
---
+++ DotFramework/Net/DotTool.PBConfig/Writer/IProtoIDWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotTool.PBConfig.Writer
+{
+    public interface IProtoIDWriter
+    {
+        //void Output(string ns,)
+    }
+}
