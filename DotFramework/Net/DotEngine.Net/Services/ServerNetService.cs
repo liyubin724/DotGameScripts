feb4a202f0be9b7e05db7beb1922ff2ae1f7c084
li.yubin <liyubin724@163.com>
2020-06-10 00:48:40 +0800

    添加基于Framework的网络接口

---
+++ DotFramework/Net/DotEngine.Net/Services/ServerNetService.cs
@@ -0,0 +1,43 @@
+﻿using DotEngine.Framework;
+using DotEngine.Net.Message;
+using DotEngine.Net.Server;
+
+namespace DotEngine.Net.Services
+{
+    public class ServerNetService : Service
+    {
+        public const string NAME = "ServerNetService";
+        private const int SERVER_NET_ID = 1;
+
+        private ServerNetListener serverNetListener = null;
+        public ServerNetService() { }
+
+        public bool IsServerRunning()
+        {
+            return serverNetListener != null;
+        }
+
+        public ServerNetListener CreateNet(IMessageParser messageParser,int port = 9999)
+        {
+            if(serverNetListener == null)
+            {
+                serverNetListener = NetManager.GetInstance().CreateServerNet(SERVER_NET_ID, port, messageParser);
+            }else
+            {
+
+            }
+            return serverNetListener;
+        }
+
+        public ServerNetListener GetNet()
+        {
+            return serverNetListener;
+        }
+
+        public void DiposeNet()
+        {
+            NetManager.GetInstance().DestroyServerNet(SERVER_NET_ID);
+            serverNetListener = null;
+        }
+    }
+}
