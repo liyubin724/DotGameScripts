331d897919fe7e2c4f31e9f6a55f6d64d824dbf0
li.yubin <liyubin724@163.com>
2020-07-03 22:55:32 +0800

    准备添加编辑器功能

---
+++ Dot/DotEditor/Timeline/ActionDataDrawer.cs
@@ -0,0 +1,16 @@
+﻿using DotEngine.Timeline.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotEditor.Timeline
+{
+    public class ActionDataDrawer
+    {
+        private ActionData actionData;
+
+
+    }
+}
--- Dot/DotEngine/Timeline/Data/Action/ActionData.cs
+++ Dot/DotEngine/Timeline/Data/Action/ActionData.cs
@@ -1,4 +1,7 @@
-﻿namespace DotEngine.Timeline.Data
+﻿using System;
+using System.Reflection;
+
+namespace DotEngine.Timeline.Data
 {
     public enum ActionPlatform
     {
@@ -13,5 +16,20 @@
         public int Id = 0;
         public ActionPlatform Platform = ActionPlatform.All;
         public float FireTime = 0.0f;
+
+        public ActionData Copy()
+        {
+            Type actionType = GetType();
+            ActionData data = (ActionData)Activator.CreateInstance(actionType);
+
+            FieldInfo[] fields = actionType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            foreach (var field in fields)
+            {
+                object value = field.GetValue(this);
+                field.SetValue(data, value);
+            }
+
+            return data;
+        }
     }
 }
